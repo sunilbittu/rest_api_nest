29fe4b5b64e178a7ae75aaa6b27accd5f225fb77
sunilb <pavanksunil@gmail.com>
2021-12-14 13:49:01 +0530

    code cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
@@ -8,7 +10,10 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI, { useFindAndModify: false }),
     TodoModule,
     AuthModule,
     UsersModule,
